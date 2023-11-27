84ebefa971f13eb24a460acaf4ea474e8acd6dc0
yunipoke <mewnya91@gmail.com>
2023-11-27 10:37:41 +0900

    change SCC

--- Graph/Scc.cpp
+++ Graph/Scc.cpp
@@ -1,105 +1,69 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-#include<set>
-using namespace std;
-int n,m,seen[2 << 17],k = 0;
-vector<int> order,component(2 << 17,-1);
-vector<vector<int>> G(2 << 17),rG(2 << 17);
-int siz[2 << 17],memo[2 << 17];
-
-void DFS(int v)
+struct SCC
 {
-    seen[v] = 1;
-    for(int nv:G[v]) if(!seen[nv]) DFS(nv);
-    order.push_back(v);
-}
+	private:
+	int N;
+	vector<vector<int>> G,RG;
+	vector<int> component,seen,order;
 
-void rDFS(int v)
-{
-    component[v] = k;
-    for(int nv:rG[v])if(component[nv] < 0) rDFS(nv);
-}
+	public:
+	
+	SCC(int N_)
+	{
+		N = N_;
+		G.resize(N);
+		RG.resize(N);
+		component.assign(N,-1);
+		seen.assign(N,0);
+	}
 
-vector<vector<int>> rebuild()
-{
-    int N = *max_element(component.begin(),component.end()) + 1;
-    vector<vector<int>> res(N);
-    set<pair<int,int>> connect;
+	void add_edge(int u,int v)
+	{
+		assert(0 <= u && u < N && 0 <= v && v < N);
+		G[u].push_back(v);
+		RG[v].push_back(u);
+	}
 
-    for(int v = 0;v < n;v++)
-    {
-        for(int nv:G[v])
-        {
-            if(component[v] != component[nv] && !connect.count(make_pair(v,nv)))
-            {
-                res[component[v]].push_back(component[nv]);
-                connect.insert(make_pair(v,nv));
-            }
-        }
-    }
+	int build(vector<vector<int>> &H)
+	{
+		for(int u = 0;u < N;u++) if(!seen[u]) dfs(u);
+		reverse(order.begin(),order.end());
+		int K = 0;
+		for(int u : order) if(component[u] < 0)
+		{
+			rdfs(u,K);
+			K++;
+		}
 
-    return res;
-}
+		H.resize(K);
+		set<pair<int,int>> connected;
+		for(int u = 0;u < N;u++) 
+		{
+			for(int v:G[u]) if(component[u] != component[v] && !connected.count(make_pair(u,v)))
+			{
+				H[component[u]].push_back(component[v]);
+				connected.insert(make_pair(u,v));
+			}
+		}
 
-int f(int v,vector<vector<int>> &reG)
-{
-    if(siz[v] > 1) return 1;
-    if(memo[v] >= 0) return memo[v];
+		return K;
+	}
 
-    for(int nv:reG[v])
-    {
-        if(f(nv,reG)) return memo[v] = 1;
-    }
+	int operator[](int u) const
+	{
+		assert(0 <= u && u < N);
+		return component[u];
+	}
 
-    return memo[v] = 0;
-}
+	void dfs(int u)
+	{
+		seen[u] = 1;
+		for(int v:G[u]) if(!seen[v]) dfs(v);
+		order.push_back(u);
+	}
 
-int main()
-{
-    cin >> n >> m;
-    for(int i = 0;i < m;i++)
-    {
-        int u,v;
-        cin >> u >> v;
-        u--; v--;
-        G[u].push_back(v);
-        rG[v].push_back(u);
-    }
-
-    for(int v = 0;v < n;v++)
-    {
-        if(!seen[v])DFS(v);
-    }
-
-    reverse(order.begin(),order.end());
-
-    for(int v:order)
-    {
-        if(component[v] < 0)
-        {
-            rDFS(v);
-            k++;
-        }
-    }
-
-    int N = 0;
-    for(int v = 0;v < n;v++)
-    {
-        siz[component[v]]++;
-        N = max(component[v],N);
-    }
-
-    auto reG = rebuild();
-    N++;
-    int res = 0;
-
-    for(int v = 0;v < N;v++) memo[v] = -1;
-
-    for(int v = 0;v < N;v++)
-    {
-        if(f(v,reG)) res += siz[v];
-    }
-
-    cout << res << endl;
-}
+	void rdfs(int u,int k)
+	{
+		component[u] = k;
+		for(int v:RG[u]) if(component[v] < 0) rdfs(v,k);
+	}
+};
