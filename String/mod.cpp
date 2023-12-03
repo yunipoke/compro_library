75b01caab42f0aef705176e08789cfc68c16806d
yunipoke <mewnya91@gmail.com>
2023-12-03 17:20:00 +0900

    change UL -> ULL

--- String/mod.cpp
+++ String/mod.cpp
@@ -1,14 +1,14 @@
 using ull = unsigned long long;
-const ull mod = (1ull << 61) - 1;
+const ull mod = (1ULL << 61) - 1;
 ull mul(ull a,ull b)
 {
 	ull au = a >> 31;
-	ull ad = a & ((1UL << 31)-1);
+	ull ad = a & ((1ULL << 31)-1);
 	ull bu = b >> 31;
-	ull bd = b & ((1UL << 31)-1);
+	ull bd = b & ((1ULL << 31)-1);
 	ull mid = au*bd + bu*ad;
 	ull midu = mid >> 30;
-	ull midd = mid & ((1UL << 30)-1);
+	ull midd = mid & ((1ULL << 30)-1);
 
 	ull res = au*bu*2 + midu + (midd << 31) + ad*bd;
 
