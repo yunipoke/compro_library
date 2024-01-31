028cc5313a84fcb4ae2632f8ded377d2e5922347
yunipoke <mewnya91@gmail.com>
2024-01-31 22:54:14 +0900

    get faster

--- Math/Convolution.cpp
+++ Math/Convolution.cpp
@@ -1,3 +1,4 @@
+
 int ceil_log2(int n)
 {
 	int res = 0;
@@ -32,47 +33,136 @@ template<class mint> void Butterfly(vector<mint> &a,bool inverse = false)
 		}
 	}
 
-	for(int i = 0;i < N;i++)
+	if(!inverse)
 	{
-		int rev = 0;
-		int cur = i;
-		for(int j = 0;j < H;j++)
+		int width = N;
+		int log = H;
+		const mint im = w[2];
+		while(width > 1)
 		{
-			rev = (rev << 1) | (cur & 1);
-			cur >>= 1;
-		}
-		if(rev > i)
-		{
-			swap(a[i],a[rev]);
+			mint cur = w[log];
+
+			if(width == 2)
+			{
+				int offset = width >> 1;
+				for(int i = 0;i < N;i += width)
+				{
+					mint root = 1;
+					for(int j = i;j < i + offset;j++)
+					{
+						mint s = a[j],t = a[j + offset];
+						a[j] = s + t;
+						a[j + offset] = (s - t) * root;
+						root *= cur;
+					}
+				}
+				width >>= 1;
+				log--;
+			}
+			else
+			{
+				int offset = width >> 2;
+				for(int i = 0;i < N;i += width)
+				{
+					mint root = 1;
+					for(int j = i;j < i + offset;j++)
+					{
+						mint root2 = root * root;
+						mint root3 = root2 * root;
+						mint s = a[j],t = a[j + offset],u = a[j + offset * 2],v = a[j + offset * 3];
+						mint spu = s + u;
+						mint smu = s - u;
+						mint tpv = t + v;
+						mint tmvim = (t - v) * im;
+						a[j] = spu + tpv;
+						a[j + offset] = (spu - tpv) * root2;
+						a[j + offset * 2] = (smu + tmvim) * root;
+						a[j + offset * 3] = (smu - tmvim) * root3;
+						root *= cur;
+					}
+				}
+				width >>= 2;
+				log -= 2;
+			}
+
 		}
 	}
-
-	int width = 2,offset = 1;
-	int log = 1;
-	while(width <= N)
+	else
 	{
-		mint cur = inverse ? iw[log] : w[log];
-
-		for(int i = 0;i < N;i += width)
+		int width = H & 1 ? 2 : 4;
+		int log = H & 1 ? 1 : 2;
+		const mint im = iw[2];
+		while(width <= N)
 		{
-			mint root = 1;
-			for(int j = i;j < i + offset;j++)
+			mint cur = iw[log];
+
+			if(width == 2)
 			{
-				mint s = a[j],t = a[j + offset];
-				a[j] = s + t * root;
-				a[j + offset] = s - t * root;
-				root *= cur;
+				int offset = width >> 1;
+				for(int i = 0;i < N;i += width)
+				{
+					mint root = 1;
+					for(int j = i;j < i + offset;j++)
+					{
+						mint s = a[j],t = a[j + offset] * root;
+						a[j] = s + t;
+						a[j + offset] = s - t;
+						root *= cur;
+					}
+				}
 			}
+			else
+			{
+				int offset = width >> 2;
+				for(int i = 0;i < N;i += width)
+				{
+					mint root = 1;
+					for(int j = i;j < i + offset;j++)
+					{
+						mint root2 = root * root;
+						mint root3 = root2 * root;
+						mint s = a[j],t = a[j + offset] * root2,u = a[j + offset * 2] * root,v = a[j + offset * 3] * root3;
+						mint spt = s + t;
+						mint smt = s - t;
+						mint upv = u + v;
+						mint umvim = (u - v) * im;
+						a[j] = spt + upv;
+						a[j + offset] = smt + umvim;
+						a[j + offset * 2] = spt - upv;
+						a[j + offset * 3] = smt - umvim;
+						root *= cur;
+					}
+				}
+			}
+
+			width <<= 2;
+			log += 2;
 		}
-		width <<= 1;
-		offset <<= 1;
-		log++;
 	}
 }
 
 template<class mint> vector<mint> Convolution(vector<mint> a,vector<mint> b)
 {
 	int N = (int)a.size(),M = (int)b.size();
+
+	if(min(N,M) <= 60)
+	{
+		vector<mint> res(N + M - 1);
+		if(N < M)
+		{
+			swap(N,M);
+			swap(a,b);
+		}
+		for(int i = 0;i < N;i++)
+		{
+			for(int j = 0;j < M;j++)
+			{
+				res[i + j] += a[i] * b[j];
+			}
+		}
+		return res;
+	}
+
 	int L = 1 << ceil_log2(N + M - 1);
 
 	a.resize(L);
@@ -80,6 +170,7 @@ template<class mint> vector<mint> Convolution(vector<mint> a,vector<mint> b)
 
 	Butterfly(a);
 	Butterfly(b);
+
 	for(int i = 0;i < L;i++)
 	{
 		a[i] *= b[i];
