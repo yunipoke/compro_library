a274f2d1f86858f81c9a014808235bbef84c66c6
yunipoke <mewnya91@gmail.com>
2024-02-09 07:07:27 +0900

    correct Math/PrimeFactorize.cpp

--- Math/Primefactorize.cpp
+++ Math/Primefactorize.cpp
@@ -3,6 +3,10 @@ vector<pair<long long,int>> PrimeFactorize(long long N)
 	vector<pair<long long,int>> res;
 	for(long long p = 2;p * p <= N;p++)
 	{
+		if(N % p != 0)
+		{
+			continue;
+		}
 		res.push_back(make_pair(p,0));
 		while(N % p == 0)
 		{
